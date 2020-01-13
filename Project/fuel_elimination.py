07c72961b5131884230b55daede215b6639bd249
Alby <mazurkovicova@gmail.com>
2020-01-13 22:23:17 +0100

    2020-01-13

--- Project/fuel_elimination.py
+++ Project/fuel_elimination.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-import numpy
+from numpy import arccos, array, dot, pi, cross, linspace, arange
+from numpy.linalg import det, norm
+from matplotlib.pyplot import axis, gca, Circle, grid, plot, show
 
 class Fuel_elimination():
     def __init__(self, fuel_tank, fuel_data):
@@ -8,15 +9,31 @@ class Fuel_elimination():
 
     def evaluate(self):
         xyr = self.fuel_data.get_coordinates()
-        for i in xyr:
-            x = i[0]
-            y = i[1]
-            r = i[2]
-        print(results)
+        raw = self.fuel_data.get_data()
+        result_list = []
+        for i in range(len(xyr)):
+            x = xyr[i][0]
+            y = xyr[i][1]
+            r = xyr[i][2]
+            processed_row = {}
+            processed_row['result'] = (self.fuel_tank.in_out(x, y, r))
+            processed_row['coord_x'] = x
+            processed_row['coord_y'] = y
+            processed_row['coord_r'] = r
+            processed_row['raw_data'] = raw[i]
+            result_list.append(processed_row)
+        return(result_list)
+
+    def separate(self):
+        with open('coordinate_file.inp', 'w') as file_in:
+            for row in self.evaluate():
+                if row['result'] == 'IN':
+                    file_in.write(str(row['raw_data']))
+            file_in.close()
 
 class Extract:
     def get_data(self):
-        with open('coordinates.inp', 'r') as file_:
+        with open('coordinates_all.inp', 'r') as file_:
             data = []
             for line in file_:
                 data.append(line)
@@ -45,60 +62,48 @@ class Fuel_tank():
         self.octagon_i = Octagon(49.77, 75)
         self.octagon_o = Octagon(87.78, 125)
     
-    def in_out_t(self, x, y, r):
-        if self.octagon_o.in_out(x,y,r) == 'IN' and self.octagon_i.in_out(x,y,r) == 'OUT':
-            print('IN')
+    def in_out(self, x, y, r):
+        if self.octagon_o.in_out_border(x,y,r) == 'IN' and self.octagon_i.in_out_border(x,y,r) == 'OUT':
+            return 'IN'
         else:
-            print('OUT')
+            return 'OUT'
 
 class Octagon():
     def __init__(self, size, intercept):
         self.size = size
         self.intercept = intercept
-        # self.f1 = -x-y+intercept
-        # self.f2 = x-y-intercept
-        # self.f3 = -x-y-intercept
-        # self.f4 = x-y+intercept
 
-    def in_out(self, x, y, r):
-            p = numpy.array([x,y])
-            a1 = numpy.array([self.intercept-self.size, self.size])
-            b1 = numpy.array([self.size, self.intercept-self.size])
-            a2 = numpy.array([self.size, self.size-self.intercept])
-            b2 = numpy.array([self.intercept-self.size, -1*self.size])
-            a3 = numpy.array([self.size-self.intercept, -1*self.size])
-            b3 = numpy.array([-1*self.size, self.size-self.intercept])
-            a4 = numpy.array([-1*self.size, self.intercept-self.size])
-            b4 = numpy.array([self.size-self.intercept, self.size])
-            dist_f1 = abs((-1*x + -1*y + self.intercept)/ (2**(1/2)))
-            dist_f2 = abs(( 1*x + -1*y - self.intercept)/ (2**(1/2)))
-            dist_f3 = abs((-1*x + -1*y - self.intercept)/ (2**(1/2)))
-            dist_f4 = abs(( 1*x + -1*y + self.intercept)/ (2**(1/2)))
-            dist_h1 = abs( 1*self.size - y)
-            dist_h2 = abs(-1*self.size - y)
-            dist_v1 = abs( 1*self.size - x)
-            dist_v2 = abs(-1*self.size - x)        
+    def in_out_border(self, x, y, r):
+            p = array([x,y])
+            a1 = array([self.intercept-self.size, self.size])
+            a2 = array([self.size, self.intercept-self.size])
+            a3 = array([self.size, self.size-self.intercept])
+            a4 = array([self.intercept-self.size, -1*self.size])
+            a5 = array([self.size-self.intercept, -1*self.size])
+            a6 = array([-1*self.size, self.size-self.intercept])
+            a7 = array([-1*self.size, self.intercept-self.size])
+            a8 = array([self.size-self.intercept, self.size])
+            oct_pts = [a1, a2, a3, a4, a5, a6, a7, a8]
+            for oct_pt in range(7):
+                if all(oct_pts[oct_pt] == p) or all(oct_pts[(oct_pt+1)%8] == p):
+                    distance = 0
+                elif arccos(dot((p - oct_pts[oct_pt]) / norm(p - oct_pts[oct_pt]), (oct_pts[(oct_pt+1)%8] - oct_pts[oct_pt]) / norm(oct_pts[(oct_pt+1)%8] - oct_pts[oct_pt]))) > pi / 2:
+                    distance = norm(p - oct_pts[oct_pt])
+                elif arccos(dot((p - oct_pts[(oct_pt+1)%8]) / norm(p - oct_pts[(oct_pt+1)%8]), (oct_pts[oct_pt] - oct_pts[(oct_pt+1)%8]) / norm(oct_pts[oct_pt] - oct_pts[(oct_pt+1)%8]))) > pi / 2:
+                    distance = norm(p - oct_pts[(oct_pt+1)%8])
+                else:
+                    distance = norm(cross(oct_pts[oct_pt]-oct_pts[(oct_pt+1)%8], oct_pts[oct_pt]-p))/norm(oct_pts[(oct_pt+1)%8]-oct_pts[oct_pt])
+                if abs(distance) < r:
+                    return 'BORDER'
             if  (
                     -1*self.size < x < self.size and -1*self.size < y < self.size and
                     #comparing fuel coordinates with vertical and horizontal borders of octagon
-                    numpy.cross(p-a1, b1-a1) > 0 and numpy.cross(p-a2, b2-a2) > 0 and numpy.cross(p-a3, b3-a3) > 0 and numpy.cross(p-a4, b4-a4) > 0
+                    cross(p-a1, a2-a1) > 0 and cross(p-a3, a4-a3) > 0 and cross(p-a5, a6-a5) > 0 and cross(p-a7, a8-a7) > 0
                     #comparing fuel coordinates with diagonal borders of octagon
                 ):
-                if r > dist_f1 and r > dist_f2 and r > dist_f3 and r > dist_f4 and r > dist_h1 and r > dist_h2 and r > dist_v1 and r > dist_v2:
-                #comparing radius with distance between fuel coordinates and borders of octagon
-                    return 'BORDER'
-                else:
-                    return 'IN'
+                return 'IN'
             else:
-                if  (
-                        ((r+self.size) > abs(x) and self.size-self.intercept < y < self.intercept-self.size) or
-                        ((r+self.size) > abs(y) and self.size-self.intercept < x < self.intercept-self.size) or
-                        (r > dist_f1 or r > dist_f2 or r > dist_f3 or r > dist_f4) and
-                        -1*self.size < x < self.size and -1*self.size < y < self.size
-                    ):
-                        return 'BORDER'
-                else:
-                    return 'OUT'
+                return 'OUT'
 
 # class Circle():
 #     def __init__(self, pos_x, pos_y, radius):
@@ -106,50 +111,45 @@ class Octagon():
 #         self.pos_y = pos_y
 #         self.radius = radius
 
-fuel_tank_0 = Fuel_tank()
-fuel_tank_0.in_out_t(-26, 51, 2.9925)
-# input_data = Extract()
-# output = Fuel_elimination(fuel_tank_0, input_data)
+class Vizualization():
+    def __init__(self, fuel_elimination, fuel_data):
+        self.fuel_elimination = fuel_elimination
+        self.fuel_data = fuel_data
+
+    def plotting(self):
+        self.background()
+        self.fuel_cells()
+        show()
 
 
+    def background(self):
+        ax = gca()
+        axis('square')
+        ax.set_xticks(arange(-100, 125, 25))
+        ax.set_yticks(arange(-100, 125, 25))
+        ax.tick_params(axis='both', which='major', labelsize=5)
+        grid(True)
+
+
+    def fuel_cells(self):
+        ax = gca()
+        for f in self.fuel_elimination.evaluate():
+            viz_x = f['coord_x']
+            viz_y = f['coord_y']
+            viz_r = f['coord_r']
+            if f['result'] == 'IN':
+                fuel = Circle((viz_x, viz_y), viz_r, color='green')
+            # else:
+            #     fuel = Circle((viz_x, viz_y), viz_r, color='red')
+                ax.add_artist(fuel)
+
+
+fuel_tank = Fuel_tank()
+input_data = Extract()
+output = Fuel_elimination(fuel_tank, input_data)
 # output.evaluate()
-# octagon_i = Octagon(49.77, 75)
-# octagon_o = Octagon(87.78, 125)
-# octagon_o.in_out(42, 80, 2.9925)
+# output.separate()
+graph = Vizualization(output, input_data)
+graph.plotting()
 
-# output = Extract()
-# output.get_coordinates()
-
-# class Vizualization():
-#     def grid(self):
-#         ax = plt.gca()
-#         ax.set_xticks(numpy.arange(-250, 250, 25))
-#         ax.set_yticks(numpy.arange(-250, 250, 25))
-#         ax.tick_params(axis='both', which='major', labelsize=5)
-#         plt.axis('square')
-#         plt.grid(True)
-#         fuel = plt.Circle(output.get_coordinates()[0], output.get_coordinates()[1], output.get_coordinates()[2])
-#         ax.add_artist(fuel)
-#         plt.show()
-
-
-# ax = plt.gca()
-# ax.set_xticks(numpy.arange(-250, 250, 25))
-# ax.set_yticks(numpy.arange(-250, 250, 25))
-# ax.tick_params(axis='both', which='major', labelsize=5)
-# plt.axis('square')
-# plt.grid(True)
-# fuel = plt.Circle((x, y), r)
-# ax.add_artist(fuel)
-
-# plt.show()        
-
-# # class Drawing_fuel(Vizualization):
-# #     def inside(self):
-# #         print(fuel)
-# #         # ax.add_artist(fuel)
-# #         # plt.show()
-
-# plot = Vizualization()
-# plot.grid()
 
