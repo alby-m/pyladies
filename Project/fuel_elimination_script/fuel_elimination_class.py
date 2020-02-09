ed3ac496e2a4aa349ec8c61a919d435eba906d31
Alby <mazurkovicova@gmail.com>
2020-02-09 17:42:45 +0100

    optimalization of the duration of running the programm

--- Project/fuel_elimination_script/fuel_elimination_class.py
+++ Project/fuel_elimination_script/fuel_elimination_class.py
@@ -2,13 +2,15 @@ class Fuel_elimination():
     def __init__(self, fuel_tank, fuel_data):
         self.fuel_tank = fuel_tank
         self.fuel_data = fuel_data
+        self.result_list = []
 
     def evaluate(self):
+        if len(self.result_list) != 0:
+            return(self.result_list)
         #saving list of data with fuel coordinates into variable
         xyr = self.fuel_data.get_coordinates()
         #saving raw data into variable
         raw = self.fuel_data.get_data()
-        result_list = []
         #creating dictionary consisiting of resulting status IN or OUR, coordinates and originate line
         for i in range(len(xyr)):
             x = xyr[i][0]
@@ -20,10 +22,10 @@ class Fuel_elimination():
             processed_row['coord_y'] = y
             processed_row['coord_r'] = r
             processed_row['raw_data'] = raw[i]
-            result_list.append(processed_row)
-            if int((i % ((len(xyr))/20))) == 0:
-                print('Progress: '+str(round((i/len(xyr))*100))+' percent done.')
-        return(result_list)
+            self.result_list.append(processed_row)
+            if int((i % ((len(xyr))/20))) == 0 or int(i+1) == len(xyr):
+                print('Data evaluation: '+str(round((i/len(xyr))*100))+' percent done.')
+        return(self.result_list)
 
     def separate(self, file_name, status):
         #new file creation with only lines marked with selected status
